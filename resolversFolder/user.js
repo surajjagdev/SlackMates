c496ef5d6a85798f16313802c0dd6aa58945411c
Suraj Jagdev <suraj_57@hotmail.com>
2019-08-25 13:07:43 -0400

    added bycrpt to hash register user password

--- resolversFolder/user.js
+++ resolversFolder/user.js
@@ -1,7 +1,16 @@
+import bcrypt from 'bcrypt';
 export default {
   //username,email, password
   Mutation: {
-    register: (parent, args, { db }) => db.User.create(args)
+    register: async (parent, { password, ...otherArgs }, { db }) => {
+      try {
+        const hashedPassword = await bcrypt.hash(password, 11);
+        await db.User.create({ ...otherArgs, password: hashedPassword });
+        return true;
+      } catch (error) {
+        return false;
+      }
+    }
   },
   Query: {
     getUser: (parent, { id }, { db }) => db.User.findOne({ where: { id } }),
