039bd01648a17a74a8cb20306f398cc3a020cef9
Suraj Jagdev <suraj_57@hotmail.com>
2019-08-27 14:27:28 -0400

    new fn

--- resolversFolder/team.js
+++ resolversFolder/team.js
@@ -1,13 +1,18 @@
+import formateErrors from '../formateErrors.js';
 export default {
   Mutation: {
     createTeam: async (parent, args, { db, user }) => {
       try {
         //also will need owner inferred via JWT spread ..args, owner:user.id
         await db.Team.create({ ...args, owner: user.id });
-        return true;
+        return {
+          ok: true
+        };
       } catch (err) {
-        console.log(err);
-        return false;
+        return {
+          ok: false,
+          errors: formateErrors(err, db)
+        };
       }
     }
   }
