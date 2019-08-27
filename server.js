3db371ac463d085392cb4fd4ccc03d7b38575d4a
Suraj Jagdev <suraj_57@hotmail.com>
2019-08-27 14:27:50 -0400

    passing in jwt tokens and verifying them

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 if (process.env.NODE_ENV !== 'production') require('dotenv').config();
 const express = require('express');
+const jwt = require('jsonwebtoken');
+const { refreshTokens } = require('./auth.js');
 const { ApolloServer, gql } = require('apollo-server-express');
 const path = require('path');
 const {
@@ -10,9 +12,35 @@ const {
 const db = require('./models');
 const cors = require('cors');
 const app = express();
-app.use(cors('*'));
-
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 3001;
+const addUser = async (req, res, next) => {
+  const token = req.headers['token'];
+  console.log('token: ', token);
+  if (token) {
+    try {
+      const { user } = jwt.verify(token, process.env.JWTSECRET);
+      req.user = user;
+      console.log('req.user: ', req.user);
+    } catch (err) {
+      console.log('err: ', err);
+      const refreshToken = req.headers['refreshtoken'];
+      const newTokens = await refreshTokens(
+        token,
+        refreshToken,
+        db,
+        process.env.JWTSECRET,
+        process.env.JWTSECRET2
+      );
+      if (newTokens.token && newTokens.refreshToken) {
+        res.set('Access-Control-Expose-Headers', 'token, refreshtoken');
+        res.set('token', newTokens.token);
+        res.set('refreshtoken', newTokens.refreshToken);
+      }
+      req.user = newTokens.user;
+    }
+  }
+  next();
+};
 const endpoint = '/graphql';
 //mash together typeDefs and resolvers.
 const types = fileLoader(path.join(__dirname, './schemaFolder'));
@@ -24,13 +52,13 @@ const resolvers = mergeResolvers(resolversArray);
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  context: {
-    db,
-    user: {
-      id: 1
-    },
-    SECRET: process.env.JWTSECRET,
-    SECRET2: process.env.JWTSECRET2
+  context: async ({ req }) => {
+    return {
+      db,
+      user: req.user,
+      SECRET: process.env.JWTSECRET,
+      SECRET2: process.env.JWTSECRET2
+    };
   },
   playground: {
     endpoint,
@@ -39,6 +67,8 @@ const server = new ApolloServer({
     }
   }
 });
+app.use(cors('*'));
+app.use(addUser);
 server.applyMiddleware({ app });
 db.sequelize.sync().then(() => {
   app.listen(port, () => {
