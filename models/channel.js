b6ca06d055123a72df6f7d3c2e5e68f0e2e90002
Suraj Jagdev <suraj_57@hotmail.com>
2019-09-02 16:09:57 -0400

    public property false default

--- models/channel.js
+++ models/channel.js
@@ -5,7 +5,8 @@ module.exports = function(sequelize, DataTypes) {
       type: DataTypes.STRING
     },
     public: {
-      type: DataTypes.BOOLEAN
+      type: DataTypes.BOOLEAN,
+      defaultValue: false
     }
   });
   return Channel;
