4833cd6f7dc41f981e805996b75a739b7316774b
Suraj Jagdev <suraj_57@hotmail.com>
2019-09-04 20:14:21 -0400

    check if team owner is also user

--- resolversFolder/channel.js
+++ resolversFolder/channel.js
@@ -2,19 +2,36 @@ import formateErrors from '../formateErrors.js';
 import requiresAuth from '../permissions.js';
 export default {
   Mutation: {
-    createChannel: requiresAuth.createResolver(async (parent, args, { db }) => {
-      try {
-        const channel = await db.Channel.create(args);
-        return {
-          ok: true,
-          channel
-        };
-      } catch (err) {
-        return {
-          ok: false,
-          errors: formateErrors(err, db)
-        };
+    createChannel: requiresAuth.createResolver(
+      async (parent, args, { db, user }) => {
+        try {
+          const team = await db.Team.findOne(
+            { where: { id: args.teamId } },
+            { raw: true }
+          );
+          if (team.owner !== user.id) {
+            return {
+              ok: false,
+              errors: [
+                {
+                  path: 'name',
+                  message: 'Must be owner of team to create channels'
+                }
+              ]
+            };
+          }
+          const channel = await db.Channel.create(args);
+          return {
+            ok: true,
+            channel
+          };
+        } catch (err) {
+          return {
+            ok: false,
+            errors: formateErrors(err, db)
+          };
+        }
       }
-    })
+    )
   }
 };
