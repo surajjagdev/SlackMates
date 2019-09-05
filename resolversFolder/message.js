78589e3caa21649c2d0d8382fa1edd938c5bcad6
Suraj Jagdev <suraj_57@hotmail.com>
2019-09-05 17:04:18 -0400

    creating message, and doing message queries for each channel

--- resolversFolder/message.js
+++ resolversFolder/message.js
@@ -1,13 +1,27 @@
+//import formateErrors from '../formateErrors.js';
+import requiresAuth from '../permissions.js';
 export default {
-  Mutation: {
-    createMessage: async (parent, args, { db, user }) => {
-      try {
-        await db.Message.create({ ...args, userId: user.id });
-        return true;
-      } catch (err) {
-        console.log(err);
-        return false;
+  Query: {
+    messages: requiresAuth.createResolver(
+      async (parent, { channelId }, { db, user }) => {
+        db.Message.findAll({ where: { channelId } }, { raw: true });
       }
-    }
+    )
+  },
+  Mutation: {
+    createMessage: requiresAuth.createResolver(
+      async (parent, args, { db, user }) => {
+        try {
+          await db.Message.create({ ...args, userId: user.id });
+          return true;
+        } catch (err) {
+          console.log(err);
+          return false;
+        }
+      }
+    )
   }
 };
+/*Query:{
+   messages:async(parent, args, {db, user})=>[//]
+ },*/
--- schemaFolder/message.js
+++ schemaFolder/message.js
@@ -3,7 +3,11 @@ type Message{
   id:Int!,
   text:String!,
   user:User!,
-  channel:Channel!
+  channel:Channel!,
+  createdAt:String!
+}
+type Query{
+  messages(channelId:Int!):[Message!]!
 }
 type Mutation{
   createMessage(channelId:Int!, text:String!):Boolean!
