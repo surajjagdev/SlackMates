dd224e993cf622ce6e8f8280c921884105cd5105
Suraj Jagdev <suraj_57@hotmail.com>
2019-09-08 13:44:23 -0400

    permissions for team acess sending messages backend

--- permissions.js
+++ permissions.js
@@ -17,13 +17,21 @@ export default createResolver((parent, args, { user }) => {
     throw new Error('not authenticated');
   }
 });
-//double check to admin level
-//check if user logged then check is user is admin
-/*
-export requiresAdmin = requiresAuth.createResolver((parent, args, { user }) => {
-  if (!contex.user.isAdmin) {
-    throw new Error('Requires Admin access!');
-  }
-});
+export const requiresTeamAccess = createResolver(
+  async (parent, { channelId }, { user, db }) => {
+    if (!user || !user.user.id) {
+      throw Error('Not Authenticated');
+    }
+    // check if part of the team
+    const channel = await db.Channel.findOne({ where: { id: channelId } });
 
-*/
+    const member = await db.Member.findOne({
+      where: { teamId: channel.teamId, userId: user.user.id }
+    });
+    if (!member) {
+      throw new Error(
+        "You have to be a member of the team to subcribe to it's messages"
+      );
+    }
+  }
+);
