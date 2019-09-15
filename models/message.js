0f83f05d7fbd63f16543b2048713c601c2528c67
Suraj Jagdev <suraj_57@hotmail.com>
2019-09-15 16:55:37 -0400

    made columns to save filetype and url for file uploads

--- models/message.js
+++ models/message.js
@@ -3,6 +3,12 @@ module.exports = function(sequelize, DataTypes) {
   const Message = sequelize.define('message', {
     text: {
       type: DataTypes.STRING
+    },
+    url: {
+      type: DataTypes.STRING
+    },
+    filetype: {
+      type: DataTypes.STRING
     }
   });
   return Message;
