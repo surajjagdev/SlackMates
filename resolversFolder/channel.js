ccde4db042442e4e38f767122374f85a727e3034
Suraj Jagdev <suraj_57@hotmail.com>
2019-09-21 15:07:52 -0400

    create a channel for dm and/or group messaging

--- resolversFolder/channel.js
+++ resolversFolder/channel.js
@@ -2,6 +2,51 @@ import formateErrors from '../formateErrors.js';
 import requiresAuth from '../permissions.js';
 export default {
   Mutation: {
+    getOrCreateChannel: requiresAuth.createResolver(
+      async (parent, { teamId, members }, { db, user }) => {
+        const newMemberArray = [];
+        members.push(user.id);
+        // check if dm channel already exists with these members
+        const [data, result] = await db.sequelize.query(
+          `
+      select c.id 
+      from channels as c, private_members pc 
+      where pc.channel_id = c.id and c.directmessage = true and c.public = false and c.team_id = ${teamId}
+      group by c.id 
+      having array_agg(pc.user_id) @> Array[:members]::uuid[] and count(pc.user_id) = ${members.length};
+      `,
+          {
+            replacements: { members: members },
+            raw: true
+          }
+        );
+
+        console.log(data, result);
+
+        if (data.length) {
+          return data[0].id;
+        }
+
+        const channelId = await db.sequelize.transaction(async transaction => {
+          const channel = await db.Channel.create(
+            {
+              name: 'hello',
+              public: false,
+              directmessage: true,
+              teamId
+            },
+            { transaction }
+          );
+
+          const cId = channel.dataValues.id;
+          const pcmembers = members.map(m => ({ userId: m, channelId: cId }));
+          await db.PrivateMember.bulkCreate(pcmembers, { transaction });
+          return cId;
+        });
+
+        return channelId;
+      }
+    ),
     createChannel: requiresAuth.createResolver(
       async (parent, args, { db, user }) => {
         try {
