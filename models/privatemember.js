af4eb47bf7f825478cf7035b92db5cd55b88b34c
Suraj Jagdev <suraj_57@hotmail.com>
2019-09-21 15:06:28 -0400

    explict type uuid creation

--- models/privatemember.js
+++ models/privatemember.js
@@ -1,5 +1,13 @@
 //member schema
 module.exports = function(sequelize, DataTypes) {
-  const PrivateMember = sequelize.define('private_member', {});
+  const PrivateMember = sequelize.define('private_member', {
+    user_id: {
+      type: DataTypes.UUID,
+      allowNull: false,
+      validate: {
+        notNull: true
+      }
+    }
+  });
   return PrivateMember;
 };
