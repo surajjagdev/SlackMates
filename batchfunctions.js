302beef8e579cf848b157ee67cdba4de8d653ea3
Suraj Jagdev <suraj_57@hotmail.com>
2019-09-26 15:38:40 -0400

    batch functions, for selection on channe joins and teams

---
+++ batchfunctions.js
@@ -0,0 +1,20 @@
+export const channelBatcher = async (ids, db, user) => {
+  const results = await db.sequelize.query(
+    'select distinct on (id) * from channels as c left outer join private_members as pm on c.id=pm.channel_id where c.team_id in (:teamIds) and (c.public=true or pm.user_id=:userId) ',
+    {
+      replacements: { teamIds: ids, userId: user.id },
+      model: db.Channel,
+      raw: true
+    }
+  );
+  const data = {};
+  //group by team;
+  results.forEach(r => {
+    if (data[r.team_id]) {
+      data[r.team_id].push(r);
+    } else {
+      data[r.team_id] = [r];
+    }
+  });
+  return ids.map(id => data[id]);
+};
