0c693ff6399dc42bd490ccc25ebfd9e83676393f
Suraj Jagdev <suraj_57@hotmail.com>
2019-09-26 15:04:26 -0400

    findall messages with optional options to find where created at less than certain period

--- resolversFolder/message.js
+++ resolversFolder/message.js
@@ -3,6 +3,12 @@ import requiresAuth, { requiresTeamAccess } from '../permissions.js';
 import { withFilter } from 'graphql-subscriptions';
 import pubsub from '../pubsub.js';
 import fs from 'fs';
+import { GraphQLScalarType } from 'graphql';
+import { Kind } from 'graphql/language';
+import Sequelize from 'sequelize';
+import { sequelize } from '../models/index.js';
+const moment = require('moment');
+const Op = Sequelize.Op;
 //event name
 //listen for new channel message via pubsub
 const NEW_CHANNEL_MESSAGE = 'NEW_CHANNEL_MESSAGE';
@@ -23,6 +29,24 @@ export default {
       )
     }
   },
+  Date: new GraphQLScalarType({
+    name: 'Date',
+    description: 'date scalar type',
+    parseValue(value) {
+      console.log('parse value: ', value);
+      return new Date(value);
+    },
+    serialize(value) {
+      console.log('seralize value: ', value);
+      return new Date(value);
+    },
+    parseLiteral(ast) {
+      if (ast.kind === Kind.INT) {
+        return parseInt(ast.value, 10);
+      }
+      return null;
+    }
+  }),
   Message: {
     url: parent =>
       parent.url ? `http://localhost:3001/static/${parent.url}` : parent.url,
@@ -31,7 +55,7 @@ export default {
   },
   Query: {
     messages: requiresAuth.createResolver(
-      async (parent, { offset, channelId }, { db, user }) => {
+      async (parent, { cursor, channelId }, { db, user }) => {
         const channel = await db.Channel.findOne({
           where: { id: channelId },
           raw: true
@@ -47,16 +71,31 @@ export default {
             );
           }
         }
-        const messages = await db.Message.findAll(
+        const options = {
+          order: [['created_at', 'DESC']],
+          where: { channelId },
+          limit: 35
+        };
+        //find all elements after cursor;
+        if (cursor) {
+          console.log('cursor: ', cursor);
+          options.where.created_at = {
+            [Op.lt]: cursor
+          };
+        }
+        /*const newMessage = await db.sequelize.query(
+          'select * from messages where "messages"."createdAt" = ?',
           {
-            order: [['createdAt', 'DESC']],
-            where: { channelId },
-            limit: 35,
-            offset
-          },
-          { raw: true }
-        );
-        return messages;
+            replacements: ['2019-09-25 16:50:40.132'],
+            model: db.Message,
+            raw: false
+          }
+        );*/
+        /* const newMessage = await db.Message.findAll({
+          where: { createdAt: '2019-09-25 20:50:17.769' }
+        });
+        console.log('new message: ', newMessage);*/
+        return db.Message.findAll(options, { raw: true });
       }
     )
   },
@@ -78,8 +117,11 @@ export default {
             const stream = createReadStream();
             uploadFile(stream, filename);
           }
+          const now = moment();
+          console.log('\n, now: ', now, '\n');
           const message = await db.Message.create({
             ...messageData,
+            created_at: now.format(),
             userId: user.id
           });
           //pubsub publish: event name and schema query
